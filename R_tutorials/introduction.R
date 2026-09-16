a36e05a5d8d86c065d9aff5b14466703a02351bc
Anmol Sapkota <sapkota.anmol822@gmail.com>
2026-09-16 21:31:25 +0545

    Introduction to R programming

---
+++ R_tutorials/introduction.R
@@ -0,0 +1,56 @@
+# ============================================================
+# R PROGRAMMING - INTRODUCTION
+# File: 01_introduction.R
+# ============================================================
+
+# R is a programming language mainly used for:
+# - Statistical computing
+# - Data analysis
+# - Data visualization
+# - Machine learning
+# - Scientific computing
+
+# R is case-sensitive.
+# Example:
+# name and Name are two different variables.
+
+# ------------------------------------------------------------
+# FIRST R PROGRAM
+# ------------------------------------------------------------
+
+# print() is used to display output.
+
+print("Hello World")
+
+# We can also directly write a value.
+"Hello R Programming"
+
+# ------------------------------------------------------------
+# COMMENTS
+# ------------------------------------------------------------
+
+# Single-line comments start with #
+
+# Comments are ignored by R.
+# They are used to explain the program.
+
+# ------------------------------------------------------------
+# BASIC CALCULATIONS
+# ------------------------------------------------------------
+
+10 + 5
+10 - 5
+10 * 5
+10 / 5
+10 ^ 2
+
+# ^ is used for exponent/power.
+
+# ------------------------------------------------------------
+# MULTIPLE STATEMENTS
+# ------------------------------------------------------------
+
+print(10 + 20)
+print(50 - 25)
+print(5 * 6)
+print(100 / 4)
