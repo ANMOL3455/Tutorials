00ab7ee5132b42700953c65d4fe5ffd2606d6869
Anmol Sapkota <sapkota.anmol822@gmail.com>
2026-09-17 19:39:59 +0545

    basics

---
+++ Java_tutorials/TypeCasting.java
@@ -0,0 +1,14 @@
+class TypeCasting{
+public static void main(String... args){
+ byte b= 126;
+ int i = b;
+ System.out.println("The byte data is: "+b);
+ System.out.println("The interger data is: "+i);
+// for the explicit we have
+int a = 126;
+byte m = (byte)a;
+ System.out.println("The explicit byte data is: "+m);
+
+}
+
+}
