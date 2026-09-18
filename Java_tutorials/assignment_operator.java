9f7c6d193eb1e2a94c73e6d31ee7f3225673fd6e
Anmol Sapkota <sapkota.anmol822@gmail.com>
2026-09-18 18:16:06 +0545

    assignment operators final

---
+++ Java_tutorials/assignment_operator.java
@@ -0,0 +1,18 @@
+/*
+java has assignment operators and they are :
++= : for addition and storing the data to the same variable
+-= : for subtraction and storing the data to the same variable
+*= : for multiplication and storing the data to the same variable
+/=  :for division and storing the data to the same variable
+*/
+
+class assignment_operator{
+public static void main(String... args){
+	int a = 45;
+	System.out.println(a+=1);
+	System.out.println(a-=1);
+	System.out.println(a*=5);
+	System.out.println(a/=1);
+}
+
+}
